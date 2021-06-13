c215ac01db5265d2f3d1d9466e2d0d11b6429349
Peter Donker <peter@bring2mind.net>
2021-06-13 21:43:18 +0200

    Getting the task to run

--- Server/Core/Services/Github/GithubService.cs
+++ Server/Core/Services/Github/GithubService.cs
@@ -12,14 +12,14 @@ namespace Connect.LanguagePackManager.Core.Services.Github
     {
         public static List<GithubRelease> GetReleases(string org, string repo)
         {
-            return GetJsonObject<List<GithubRelease>>("repos/{org}/{repo}/releases?per_page=99");
+            return GetJsonObject<List<GithubRelease>>($"repos/{org}/{repo}/releases?per_page=99");
         }
 
         private static T GetJsonObject<T>(string relativeUrl)
         {
             var request = (HttpWebRequest)WebRequest.Create($"https://api.github.com/{relativeUrl}");
-            request.Headers.Set(HttpRequestHeader.UserAgent, "Connect.LanguagePackManager");
-            request.Headers.Add(HttpRequestHeader.Accept, "application/vnd.github.v3+json");
+            request.UserAgent = "Connect.LPM";
+            request.Accept = "application/vnd.github.v3+json";
             using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
             {
                 if (response.StatusCode == HttpStatusCode.OK)
--- Server/LanguagePackManager/Controllers/LinksController.cs
+++ Server/LanguagePackManager/Controllers/LinksController.cs
@@ -23,12 +23,29 @@ namespace Connect.LanguagePackManager.Presentation.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(PackageLink link)
+        public ActionResult Edit(int linkId, PackageLink link)
         {
+            var existing = PackageLinkRepository.Instance.GetPackageLink(ModuleContext.ModuleId, linkId);
+            if (existing == null)
+            {
+                existing = new PackageLink()
+                {
+                    ModuleId = ModuleContext.ModuleId
+                };
+            }
+            existing.Name = link.Name.Trim();
+            existing.OrgName = link.OrgName.Trim();
+            existing.RepoName = link.RepoName.Trim();
+            existing.AssetRegex = link.AssetRegex.Trim();
+            if (linkId == -1)
+            {
+                PackageLinkRepository.Instance.AddPackageLink(existing.GetPackageLinkBase(), User.UserID);
+            }
+            else
+            {
+                PackageLinkRepository.Instance.UpdatePackageLink(existing.GetPackageLinkBase(), User.UserID);
+            }
 
-            //var link = PackageLinkRepository.Instance.GetPackageLink(ModuleContext.ModuleId, linkId);
-            //if (link == null) link = new Core.Models.PackageLinks.PackageLink();
-            //return View(link);
             return View("Index");
         }
     }
